25a594fe844fbc3079bc3320ec0d85735cfb4160
ewspaan <ewspaan@gmail.com>
2022-01-13 00:56:25 +0100

    Bla 2

--- src/routes/RoutesPage.js
+++ src/routes/RoutesPage.js
@@ -4,8 +4,6 @@ import HeaderTop from "../components/organisms/header/HeaderTop";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import SignUp from "../pages/SignUp";
-import UserRoute from "./UserRoute";
-import AdminRoute from "./AdminRoute";
 import ProfilePage from "../pages/ProfilePage";
 
 
